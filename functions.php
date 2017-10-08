b007da9e21f79ab002abca9accacad75ec9969db
michaelschofield <michael@schoeyfield.com>
2017-10-08 09:41:34 -0400

    Pulls in the Roboto font.

--- functions.php
+++ functions.php
@@ -117,8 +117,9 @@ add_action( 'widgets_init', 'radian_widgets_init' );
  * Enqueue scripts and styles.
  */
 function radian_scripts() {
-	wp_enqueue_style( 'radian-style', get_template_directory_uri() . '/public/radian.css' );
 
+    wp_enqueue_style('radian-font', 'https://fonts.googleapis.com/css?family=Roboto:300,400' );
+	wp_enqueue_style( 'radian-style', get_template_directory_uri() . '/public/radian.css' );
 	wp_enqueue_script( 'radian-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
